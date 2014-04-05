ab767255e0b4b606637f73bbe7a6daa9c80f8db5
MayraDel <mayroula-92@hotmail.com>
2014-04-06 02:40:03 +0300

    SalesEmployee Added

--- src/main/EmployeeRegistry.java
+++ src/main/EmployeeRegistry.java
@@ -8,9 +8,11 @@ class EmployeeRegistry {
         SecretaryEmployee sep = new SecretaryEmployee("Anna", 1100, 3, 30.5);
         GreekEmployee gem = new GreekEmployee("Manolis", 426, "Master", 10, 3);
         ae.setBonus(500);
+        SalesEmployee se = new SalesEmployee("Mayra", 600, 7, 0.5f, 32000);
         System.out.println("Employee makes " + e.printIncome() + " Euros, TechEmployee makes " + te.printIncome() + " Euros, AdminEmployee makes " + ae.printIncome() + " Euros,");
         System.out.println("SecretaryEmployee makes " + sep.printIncome() + " Euros");
         System.out.println("GreekEmployee makes " + gem.printIncome() + " Euros");
+        System.out.println(se.toString());
     }
 }
 
