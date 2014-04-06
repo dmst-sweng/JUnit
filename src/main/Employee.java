dcfee8259e22e0f760220a82653d822501eb73d1
Stamatis Pitsios <stamatis.pit@gmail.com>
2014-04-06 18:55:26 +0300

    Improved structure of the Employee class.  Added javadoc , the ssn attribute and some functions.

--- src/main/Employee.java
+++ src/main/Employee.java
@@ -1,17 +1,75 @@
 package main;
 
+/**
+ * Every instance of this class represents a base Employee.
+ *  
+ * @author dmst-sweng
+ */
 public class Employee {
 
+	/**
+	 * The employee's name.
+	 */
     protected String name;
+    
+    /**
+     * The employee's base salary.
+     */
     protected double baseSalary;
-
+    
+    /**
+     * The employee's ssn. Used to uniquely identify an employee.
+     */
+    protected String ssn;
+    
+    
+    /**
+     * Base constructor.
+     * 
+     * @param n The name of the employee.
+     * @param s The base salary of the employee.
+     */
     public Employee(String n, double s) { 
-	name = n;
-	baseSalary = s;
+    	this(n , s , "-1");
+    }
+    
+    /**
+     * Overloaded constructor
+     * 
+     * @param n The name of the employee.
+     * @param s The base salary of the employee.
+     * @param ssn The ssn of the employee.
+     */
+    public Employee(String n , double s , String ssn) {
+    	this.name = n;
+    	this.baseSalary = s;
+    	this.ssn = ssn;
     }
 
+    /**
+     * Returns the income of the employee.
+     * 
+     * @return The base salary.
+     */
     public double printIncome() {
-	return baseSalary;
+    	return this.baseSalary;
+    }
+    
+    /**
+     * Returns the employee's ssn
+     * 
+     * @return The ssn.
+     */
+    public String getSSN() {
+    	return this.ssn;
+    }
+    
+    /**
+     * Sets the employee's ssn , to the value given as parameter.
+     * 
+     * @param ssn The new ssn of the employee.
+     */
+    public void setSSN(String ssn) {
+    	this.ssn = ssn;
     }
 }
-
