21ec082f9cfb5c67a7bd28b503ba6b33cd908e71
Stamatis Pitsios <stamatis.pit@gmail.com>
2014-04-12 19:42:56 +0300

    Revert "Better improvement in Employee's hashCode() function appearance."
    This reverts commit acf58afc44939bb76436395d6a48180ce90d1584.

--- src/test/EmployeeTest.java
+++ src/test/EmployeeTest.java
@@ -90,10 +90,10 @@ public class EmployeeTest {
 	@Test
 	public void testHashCode() {
 		int expected = 41 * 
-                        (41 * 
+				        (41 * 
 				          (41 + this.emp1.getName().hashCode()) + 
-				           (int)this.emp1.printIncome()) + 
-				            this.emp1.getSSN().hashCode();
+				          (int)this.emp1.printIncome()) + 
+				           this.emp1.getSSN().hashCode();
 		
 		assertEquals(expected, emp1.hashCode());
 	}
