08fd4ca67c4df77de6a324c9e69b6e47ebeaf621
isabel-mend <isabel_mendonca@yahoo.gr>
2014-04-14 16:26:01 +0300

    Added test for toString method of Sales Employee and afterTest method.

--- src/test/EmployeeTestSales.java
+++ src/test/EmployeeTestSales.java
@@ -1,9 +1,9 @@
 package test;
 
 import static org.junit.Assert.*;
-
 import main.SalesEmployee;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,6 +16,10 @@ public class EmployeeTestSales {
 		se = new SalesEmployee("Mayra", 600, 7, 0.5f, 32000);
 	}
 	
+	@After
+	public void afterTest() {
+		se = null;
+	}
 	@Test
 	public void testEmployeeIncomeTrue() {
 		assertEquals(16642, se.EmployeeIncome(7, 0.5f), 0.1);
@@ -26,4 +30,9 @@ public class EmployeeTestSales {
 		assertNotSame(6000, se.EmployeeIncome(7, 0.5f));
 	}
 
+	@Test
+	public void testSalesEmployeeToString() {
+		String exp = "Name: " + se.getName() + "\nFinal Income: " + se.EmployeeIncome(se.overTimeHours, se.commission) + " Euros";
+		assertEquals(exp, se.toString() );
+	}
 }
