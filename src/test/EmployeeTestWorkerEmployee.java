5c43cf2d1a40d2ee43fc508445fca4353c2f04e6
harisbitsakou <harisbitsakou@hotmail.com>
2014-04-15 13:54:41 +0300

    added some refinements to EmployeeTestWorkerEmployee

--- src/test/EmployeeTestWorkerEmployee.java
+++ src/test/EmployeeTestWorkerEmployee.java
@@ -1,14 +1,15 @@
 package test;
 
 import static org.junit.Assert.*;
-import main.FinancialEmployee;
+import main.WorkerEmployee;
+
 import org.junit.Test;
 
 public class EmployeeTestWorkerEmployee {
 
 	@Test
 	  public void testPrintIncome() {
-		FinancialEmployee fe = new FinancialEmployee("John", 1200, 200, 10);
-	    assertEquals(3200, fe.printIncome(), 0.1);
+		WorkerEmployee we = new WorkerEmployee("james", 1200, 200, 10);
+	    assertEquals(3200, we.printIncome(), 0.1);
 	  }
 }
