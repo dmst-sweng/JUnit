0c7c9ed4e7a1a61ab69106fdb5371edd1e546b0b
unknown <tatsisf@gmail.com>
2014-04-29 13:36:28 +0300

    Minor changes to FinancialEmployee and EmployeeTestFinancial

--- src/main/FinancialEmployee.java
+++ src/main/FinancialEmployee.java
@@ -4,19 +4,27 @@ public class FinancialEmployee extends Employee {
 	
 	private int extradays;
 	private double rate;
+	private int lateness;
+	private double latenessrate;
 
-	public FinancialEmployee(String n, double s, int ed, double r) {
+	public FinancialEmployee(String n, double s, int ed, double r, int l, double lr) {
 		super(n, s);
 		extradays = ed;
 		rate = r;
+		lateness = l;
+		latenessrate = lr;
 	}
-	
+
 	public void setExtraDays(int ed) {
 		extradays = ed;
 	}
 
+	public void setLateness(int l) {
+		 lateness = l;
+	}
+
 	public double printIncome() {
-		return baseSalary + extradays*rate;
+		return baseSalary + extradays*rate - lateness*latenessrate;
 	}
 	
 }
--- src/test/EmployeeTestFinancial.java
+++ src/test/EmployeeTestFinancial.java
@@ -8,7 +8,7 @@ public class EmployeeTestFinancial {
 
 	@Test
 	  public void testPrintIncome() {
-		FinancialEmployee fe = new FinancialEmployee("Bill", 1200, 2, 100);
-	    assertEquals(1400, fe.printIncome(), 0.1);
+		FinancialEmployee fe = new FinancialEmployee("Bill", 1200, 2, 100, 4, 20);
+	    assertEquals(1320, fe.printIncome(), 0.1);
 	  }
 }
