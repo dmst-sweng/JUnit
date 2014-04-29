b640eb622ae25999f312ee9e6992a57604c4074c
myronkaradimas <myron.kar@gmail.com>
2014-04-29 21:27:55 +0300

    Salary Increase based in Years

---
+++ src/main/SalaryYearIncreaseEmployee.java
@@ -0,0 +1,34 @@
+package main;
+
+public class SalaryYearIncreaseEmployee extends Employee {
+
+    private int year;
+
+    public SalaryYearIncreaseEmployee(String n, double s, int y) {
+        super(n, s);
+        this.year = y;
+    }
+
+    public void setYear(int y) {
+        year = y;
+    }
+
+    public int getYear() {
+        return this.year;
+     }
+    
+    public double totalYearsSalary() {
+        
+        if (year >= 20) {
+        return baseSalary + baseSalary*0.75; }
+
+        else if (year >5 AND year<20) {
+        return baseSalary + baseSalary*0.50; }
+
+        else {
+        return baseSalary + baseSalary*0.25; }
+        
+
+    }
+}
+
